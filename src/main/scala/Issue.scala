95b19da79000d5a077263cd8ec42624e1f09f878
Kevin <kevin.bra99@gmail.com>
2019-12-17 17:37:39 +0100

    case class fix, Forkee, Issue

--- src/main/scala/Issue.scala
+++ src/main/scala/Issue.scala
@@ -1,6 +1,6 @@
 
 
-case class Issue(
+class Issue(
                 assignee: Assignee,
                 assignees: Array[Assignees],
                 author_association: String,
@@ -25,3 +25,37 @@ case class Issue(
                 url: String,
                 user: User
                 )
+
+  extends Product
+    //For Spark it has to be Serializable
+    with Serializable {
+  def canEqual(that: Any) = that.isInstanceOf[Issue]
+
+  def productArity = 23 // number of columns
+
+  def productElement(idx: Int) = idx match {
+    case 0 => assignee
+    case 1 => assignees
+    case 2 => author_association
+    case 3 => body
+    case 4 => closed_at
+    case 5 => comments
+    case 6 => comments_url
+    case 7 => created_at
+    case 8 => events_url
+    case 9 => html_url
+    case 10 => id
+    case 11 => labels
+    case 12 => labels_url
+    case 13 => locked
+    case 14 => milestone
+    case 15 => number
+    case 16 => pull_request
+    case 17 => repository_url
+    case 18 => state
+    case 19 => title
+    case 20 => updated_at
+    case 21 => url
+    case 22 => user
+  }
+}
