1eaac8b76909e90a1abe5df365a48f70ddec545f
Andrea <andreborgo1998@gmail.com>
2019-12-17 17:14:49 +0100

    Modifica classe Payload per 22+ chars

--- src/main/scala/Payload.scala
+++ src/main/scala/Payload.scala
@@ -1,4 +1,6 @@
-case class Payload (
+import scala.tools.cmd.Demo
+
+class Payload (
                    action: String,
                    before: String,
                    comment: Comment,
@@ -28,3 +30,42 @@ case class Payload (
                    head: String,
                    issue: Issue
               )
+
+extends Product
+with Serializable {
+  def canEqual(that: Any) = that.isInstanceOf[Demo]
+
+  def productArity = 26
+
+  def productElement(idx: Int) = idx match {
+    case 0 => action
+    case 1 => before
+    case 2 => comment
+    case 3 => author_association
+    case 4 => body
+    case 5 => commit_id
+    case 6 => created_at
+    case 7 => diff_hunk
+    case 8 => html_url
+    case 9 => id
+    case 10 => in_reply_to_id
+    case 11 => issue_url
+    case 12 => line
+    case 13 => original_commit_id
+    case 14 => original_position
+    case 15 => path
+    case 16 => position
+    case 17 => pull_request_review_id
+    case 18 => pull_request_url
+    case 18 => updated_at
+    case 19 => url
+    case 19 => user
+    case 20 => commit
+    case 21 => description
+    case 22 => distinct_size
+    case 23 => forkee
+    case 24 => head
+    case 25 => issue
+
+  }
+}
