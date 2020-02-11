290a1d83855c801acfa35363154d1bc5da8db882
Ludovico <ludovico.lalli@gmail.com>
2020-02-11 13:09:51 +0100

    Aggiunta Payload modificato

--- src/main/scala/Payload.scala
+++ src/main/scala/Payload.scala
@@ -1,5 +1,3 @@
-import scala.tools.cmd.Demo
-
 class Payload (
                    action: String,
                    before: String,
@@ -14,42 +12,11 @@ class Payload (
                    member: Member,
                    number: Long,
                    pages: Pages,
-                    //pull_request,
+                   pull_request: PullRequest,
                    push_id: Long,
                    pusher_type: String,
                    ref: String,
                    ref_type: String,
                    release: Release,
                    size: Long
-
               )
-
-extends Product
-with Serializable {
-  def canEqual(that: Any) = that.isInstanceOf[Demo]
-
-  def productArity = 26
-
-  def productElement(idx: Int) = idx match {
-    case 0 => action
-    case 1 => before
-    case 2 => comment
-    case 3 => commit
-    case 4 => description
-    case 5 => distinct_size
-    case 6 => forkee
-    case 7 => head
-    case 8 => issue
-    case 9 => master_branch
-    case 10 => member
-    case 11 => pages
-     // case 12 => pull_request
-    case 13 =>push_id
-    case 14 => pusher_type
-    case 15 => ref
-    case 16 => ref_type
-    case 17 => release
-    case 18 => size
-
-  }
-}
