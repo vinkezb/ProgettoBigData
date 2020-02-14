9e23f1ec927bcb648d379fca76e3f9453ace937f
Andrea <andreborgo1998@gmail.com>
2020-02-14 15:16:03 +0100

    added methods to create csv file from RDD and DF + query

--- src/main/scala/classes/MainClasseAndrea.scala
+++ src/main/scala/classes/MainClasseAndrea.scala
@@ -5,39 +5,47 @@ import java.nio.file.{Files, Path, Paths, StandardCopyOption}
 import java.util.Properties
 import java.util.zip.{GZIPInputStream, ZipInputStream}
 
+import classes.Wrapper
 import com.opencsv.CSVWriter
 import org.apache.commons.io.FileUtils
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.types.StructType
 
+import scala.reflect.ClassTag
+
 object MainClasseAndrea {
   def main(args: Array[String]) = {
 
     var conf = new SparkConf().setMaster("local[2]").setAppName("CountingSheep")
-    val sc = new SparkContext(conf)
-    val sqlContext = new HiveContext(sc)
+    //    val sc = new SparkContext(conf)
+    //    val sqlContext = new HiveContext(sc)
     val schema = ScalaReflection.schemaFor[Wrapper].dataType.asInstanceOf[StructType]
     val url = "http://data.githubarchive.org/" + readProperties().getProperty("anno") + "-" + readProperties().getProperty("mese") + "-" + readProperties().getProperty("giorno") + "-" + "0.json.gz"
     val unzippedFile = connection(url) //download and unzip file
-    val jsonDF = sqlContext.read.schema(schema).json(System.getProperty("user.dir") +"\\src\\test\\resources\\test.txt")
-    import sqlContext.implicits._
+    val spark = SparkSession.builder.config(conf).getOrCreate()
+
+    val jsonDF = spark.read.schema(schema).json(System.getProperty("user.dir") +"\\src\\test\\resources\\test.txt")
+    import spark.sqlContext.implicits._
 
 
-//
-//    val actorDF = jsonDF.select("actor.*").show()
-//    val authorDF = jsonDF.select("payload.commits.author").show()
-//    val repoDF = jsonDF.select("repo").show()
-//    val typeDF = jsonDF.select("type").show()
+
+    val actorDF = jsonDF.select("actor")
+    //    val authorDF = jsonDF.select("payload.commits.author").show()
+    //    val repoDF = jsonDF.select("repo").show()
+    //    val typeDF = jsonDF.select("type").show()
 
 
-    val w = new FileWriter(System.getProperty("user.dir") +"\\src\\test\\resources\\data.csv")
-    val writer : CSVWriter = new CSVWriter(w, CSVWriter.DEFAULT_SEPARATOR)
+    //    setCSVFILEforDF("data", jsonDF)
+    setCSVFileForDF("actor", actorDF)
 
-    val jsonRDD = jsonDF.as[Wrapper_].rdd
-     val jsonRDDArray = jsonRDD.collect().map(x => x.toString)
-    writer.writeNext(jsonRDDArray)
+    //    val actorRDD = jsonDF.as[Actor].rdd
+    //    val actorRDDtoArray = actorRDD.collect().map(x => x.toString)
+
+
     //   val JsonDfActor = sqlContext.read.json("C:\\Users\\kevin\\Desktop\\json\\actor.json");
     //   val rdd = JsonDfActor.as[Actor].rdd
     //   rdd.foreach(println)
@@ -53,6 +61,21 @@ object MainClasseAndrea {
     val property = new Properties()
     property.load(new FileInputStream(new File(System.getProperty("user.dir") +"\\src\\test\\resources\\progetto.properties")))
     property
+  }
+
+  def setCSVFileForDF(nameFile: String, dataSource: DataFrame){
+
+    val w = new FileWriter(System.getProperty("user.dir") +"\\src\\test\\resources\\" + nameFile + ".csv")
+    val writer : CSVWriter = new CSVWriter(w, CSVWriter.DEFAULT_SEPARATOR)
+    writer.writeNext(dataSource.collect().map(x => x.toString()))
+    println("Ho scritto il file " + nameFile + ".csv da DF")
+  }
+
+  def setCSVFileForRDD[T: ClassTag](nameFile: String, dataSource: RDD[T]){
+    val w = new FileWriter(System.getProperty("user.dir") +"\\src\\test\\resources\\" + nameFile + ".csv")
+    val writer : CSVWriter = new CSVWriter(w, CSVWriter.DEFAULT_SEPARATOR)
+    writer.writeNext(dataSource.collect().map(x => x.toString()))
+    println("Ho scritto il file " + nameFile + ".csv da RDD")
   }
 
   def connection(url: String): Unit ={
