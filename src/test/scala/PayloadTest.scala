5993420300f7f5086a88efa1dbc507505ee740a3
Kevin <kevin.bra99@gmail.com>
2020-02-21 09:16:35 +0100

    change payload test name

--- src/test/scala/PayloadTest.scala
+++ src/test/scala/PayloadTest.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.types.StructType
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{FlatSpec, Matchers}
 
-class FinaleTest extends  FlatSpec  with Matchers {
+class PayloadTest extends  FlatSpec  with Matchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
     val conf = new SparkConf().setMaster("local[2]")
